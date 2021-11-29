64474e3a89b1e24c4e7ba71065e737273f6c9bee
Romarin <romain.rogier@epitech.eu>
2021-11-29 22:07:40 +0100

    update index.js, adding todo-list in files | ✔

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,11 @@
+// TODO: Crée une fonction asynchrone pour récuperer les datas d'un JSON | ✔
 async function getJSONData() {
     const response = await fetch("./src/json/data.json");
     const data = await response.json()
 
     data.user.forEach(info => {
         info.passion.forEach(result => {
+
             if (result === "Développement") {
                 console.log(info.name)
             }
@@ -11,6 +13,10 @@ async function getJSONData() {
 
     })
 }
+
+// TODO: Faire appparraitre un selector avec les passions récupérer dans le JSON | ✖
+// TODO: Faire des filtres avec le selector (switch, case, break) | ✖
+// TODO: Afficher le resultat sur la page web | ✖
 
 const contentText = document.createElement("p");
 getJSONData();
