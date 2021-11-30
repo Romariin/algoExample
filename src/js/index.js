b5f123198620d90f678819f03be66298d06ac19a
Romain <romainrogier2@gmail.com>
2021-11-30 11:21:45 +0100

    Faire appparraitre un selector avec les passions récupérer dans le JSON | ✔

--- src/js/index.js
+++ src/js/index.js
@@ -1,22 +1,42 @@
 // TODO: Crée une fonction asynchrone pour récuperer les datas d'un JSON | ✔
-async function getJSONData() {
-    const response = await fetch("./src/json/data.json");
-    const data = await response.json()
+function globalFunction() {
+    genSelector();
+    async function getJSONData() {
+        const response = await fetch("./src/json/data.json");
+        const data = await response.json();
+        return data;
+    }
+// TODO: Faire appparraitre un selector avec les passions récupérer dans le JSON | ✔
+    function genSelector() {
+        // ["Vélo", "Tricot", "Fashion", "Muscu", "Gaming", "Développement"] //
+        getJSONData()
+            .then(result => {
+                var catPassion = [];
+                result.forEach(result => {
+                    for (let i = 0 ; i < result.passion.length ; i++) {
+                        if (!catPassion.includes(result.passion[i])) {
+                        catPassion.push(result.passion[i]);
+                        }
+                    }
+                })
+                const divSelect = document.querySelector(".selector");
+                const selectList = document.createElement("select");
+                selectList.id = "mySelect";
+                divSelect.appendChild(selectList);
 
-    data.user.forEach(info => {
-        info.passion.forEach(result => {
+                for (let i = 0 ; i < catPassion.length ; i++) {
+                    const option = document.createElement("option");
+                    option.value = catPassion[i];
+                    option.text = catPassion[i];
+                    selectList.appendChild(option)
+                }
+            })
+    }
 
-            if (result === "Développement") {
-                console.log(info.name)
-            }
-        })
-
-    })
 }
-
-// TODO: Faire appparraitre un selector avec les passions récupérer dans le JSON | ✖
 // TODO: Faire des filtres avec le selector (switch, case, break) | ✖
+
 // TODO: Afficher le resultat sur la page web | ✖
 
 const contentText = document.createElement("p");
-getJSONData();
+globalFunction();
