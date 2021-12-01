341419b79e873883fb28405f0efd4fcf7f833793
Romarin <romain.rogier@epitech.eu>
2021-12-01 11:14:11 +0100

    yessai

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,13 @@
 // TODO: Crée une fonction asynchrone pour récuperer les datas d'un JSON | ✔
 function globalFunction() {
     genSelector();
+
     async function getJSONData() {
         const response = await fetch("./src/json/data.json");
         const data = await response.json();
         return data;
     }
+
 // TODO: Faire appparraitre un selector avec les passions récupérer dans le JSON | ✔
     function genSelector() {
         // ["Vélo", "Tricot", "Fashion", "Muscu", "Gaming", "Développement"] //
@@ -13,9 +15,9 @@ function globalFunction() {
             .then(result => {
                 var catPassion = [];
                 result.forEach(result => {
-                    for (let i = 0 ; i < result.passion.length ; i++) {
+                    for (let i = 0; i < result.passion.length; i++) {
                         if (!catPassion.includes(result.passion[i])) {
-                        catPassion.push(result.passion[i]);
+                            catPassion.push(result.passion[i]);
                         }
                     }
                 })
@@ -23,20 +25,43 @@ function globalFunction() {
                 const selectList = document.createElement("select");
                 selectList.id = "mySelect";
                 divSelect.appendChild(selectList);
+                const option = document.createElement("option");
+                option.text = "Recherche par passion";
+                selectList.appendChild(option);
+                const actualSelected = document.getElementById("mySelect");
 
-                for (let i = 0 ; i < catPassion.length ; i++) {
+                for (let i = 0; i < catPassion.length; i++) {
                     const option = document.createElement("option");
                     option.value = catPassion[i];
                     option.text = catPassion[i];
                     selectList.appendChild(option)
                 }
+                // TODO: ----------------------------------------------------------------
+                // TODO: Afficher le resultat sur la page web | ✔
+                const resultDiv = document.querySelector(".result")
+                const balise = resultDiv.innerHTML;
+                actualSelected.addEventListener('change', (event) => {
+                    console.log(balise)
+                    result.forEach(info => {
+                        info.passion.forEach(result => {
+                            if (result === actualSelected.value) {
+                                const divContent = document.createElement("div");
+                                const titleContent = document.createElement("h1");
+                                const pContent = document.createElement("p");
+
+                                titleContent.innerText = info.name
+                                pContent.innerText = `Age: ${info.age}\n Ville: ${info.ville}\nPassion(s): ${info.passion}`
+                                divContent.appendChild(titleContent);
+                                divContent.appendChild(pContent);
+                                resultDiv.appendChild(divContent);
+                            }
+                        })
+                    })
+                })
             })
     }
-
 }
-// TODO: Faire des filtres avec le selector (switch, case, break) | ✖
-
-// TODO: Afficher le resultat sur la page web | ✖
-
-const contentText = document.createElement("p");
 globalFunction();
+
+
+
